87d2132c3c8eac16f8461e0dc88da2befd545c61
temiloluwua <temiadeleye1@gmail.com>
2025-09-01 20:47:23 -0600

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false
+    },
+    watch: {
+      usePolling: true,
+      interval: 1000
+    }
+  }
   base: '/',
   build: {
     outDir: 'dist',
